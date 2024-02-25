0c57530f46a8c94a25dbeff0444a92154b9ea303
Denis Filatov <denis.filatov@ideus.biz>
2024-02-25 23:01:10 +0200

    feat: add websocket server response

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
 wsServer.listen(WS_PORT, () => {
-    console.log(`WS server started on port ${WS_PORT}`);
+    console.log(`WS server started on port: ${WS_PORT}`);
 });
--- src/ws_server/index.js
+++ src/ws_server/index.js
@@ -8,8 +8,9 @@ wss.on('connection', function connection(ws) {
     console.log('Client connected');
 
     ws.on('message', function incoming(message) {
-        console.log('Income Message %s', message);
-        ws.send('Some response');
+        console.log('Income Message: %s', message);
+        const resp = ' {"type":"reg","data":"{\\"name\\":\\"User\\",\\"index\\":\\"1\\"}","id":0}\n'
+        ws.send(resp);
     });
 
     ws.on('close', function() {
